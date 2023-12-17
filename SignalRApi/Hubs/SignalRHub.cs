4e272ef59fc45b87fa3f2469c10805ad5fe57f35
Vasfi Ölmez <58458944+vasfiolmez@users.noreply.github.com>
2023-12-17 22:27:51 +0300

    istatistik verilerinin getirilmesi.

--- SignalRApi/Hubs/SignalRHub.cs
+++ SignalRApi/Hubs/SignalRHub.cs
@@ -1,16 +1,54 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using SignalR.BusinessLayer.Abstract;
 using SignalR.DataAccessLayer.Concrete;
 using SignalRApi.Entitylayer.Entities;
 
 namespace SignalRApi.Hubs
 {
-    public class SignalRHub:Hub
+    public class SignalRHub : Hub
     {
-        SignalRContext context =new SignalRContext();
-        public async Task SendCategoryCount()
+        private readonly ICategoryService _categoryService;
+        private readonly IProductService _productService;
+
+        public SignalRHub(ICategoryService categoryService, IProductService productService)
         {
-            var value=context.Categories.Count();
-            await Clients.All.SendAsync("ReceiveCategoryCount", value);
+            _categoryService = categoryService;
+            _productService = productService;
         }
+
+        public async Task SendStatistic()
+        {
+            var value = _categoryService.TCategoryCount();
+            await Clients.All.SendAsync("ReceiveCategoryCount", value);
+
+            var value2 = _productService.TProductCount();
+            await Clients.All.SendAsync("ReceiveProductCount", value2);
+
+            var value3 = _categoryService.TActiveCategoryCount();
+            await Clients.All.SendAsync("ReceiveActiveCategoryCount", value3);
+
+            var value4 = _categoryService.TPassiveCategoryCount();
+            await Clients.All.SendAsync("ReceivePassiveCategoryCount", value4);
+
+            var value5 = _productService.TProductCountByCategoryNameHamburger();
+            await Clients.All.SendAsync("ReceiveProductCountByCategoryNameHamburger",value5);
+
+            var value6 = _productService.TProductCountByCategoryNameDrink();
+            await Clients.All.SendAsync("ReceiveProductCountByCategoryNameDrink", value6);
+
+            var value7 = _productService.TProductPriceAvg();
+            await Clients.All.SendAsync("ReceiveProductPriceAvg", value7.ToString("0.00"+ "₺"));
+
+            var value8=_productService.TProductNameByMaxPrice();
+            await Clients.All.SendAsync("ReceiveProductByMaxPrice",value8);
+
+            var value9 = _productService.TProductNameByMinPrice();
+            await Clients.All.SendAsync("ReceiveProductByMinPrice", value9);
+
+            var value10 = _productService.TProductAvgPriceByHamburger();
+            await Clients.All.SendAsync("ReceiveProductAvgPriceByHamburger", value10);
+
+        }
+
     }
 }
