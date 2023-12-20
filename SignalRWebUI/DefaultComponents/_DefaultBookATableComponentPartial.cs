35ed07330178936c7636bfcb9507c803b39bfed5
Vasfi Ölmez <58458944+vasfiolmez@users.noreply.github.com>
2023-12-20 22:47:28 +0300

    uı homepage

---
+++ SignalRWebUI/DefaultComponents/_DefaultBookATableComponentPartial.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SignalRWebUI.DefaultComponents
+{
+    public class _DefaultBookATableComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        { return View(); }
+    }
+}
---
+++ SignalRWebUI/DefaultComponents/_DefaultOurMenuComponentPartial.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SignalRWebUI.DefaultComponents
+{
+    public class _DefaultOurMenuComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {  return View(); }
+    }
+}
