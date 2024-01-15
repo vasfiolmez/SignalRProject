7066f3766466e28730740c1ea92f41924ee0bc08
Vasfi Ölmez <58458944+vasfiolmez@users.noreply.github.com>
2024-01-15 14:02:50 +0300

    sunum

--- SignalRWebUI/Controllers/AboutUIController.cs
+++ SignalRWebUI/Controllers/AboutUIController.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using SignalRWebUI.Dtos.AboutDtos;
 
 namespace SignalRWebUI.Controllers
 {
+    [AllowAnonymous]
     public class AboutUIController : Controller
     {
         private readonly IHttpClientFactory _httpClientFactory;
